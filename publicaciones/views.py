cad9e9669dfc0ed7d7f3aff7a812493cbbc77c75
alegrelias26 <elias.n.alegre26-1@gmail.com>
2026-05-08 12:00:40 -0300

    Se zrregla InicioView y se eliminan comentarios

--- publicaciones/views.py
+++ publicaciones/views.py
@@ -5,9 +5,8 @@ class InicioView(TemplateView):
     template_name = 'publicaciones/inicio.html'
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        #TODO: arreglar esto
-        context["titulo"] = "..."
-        context["mensaje"] = "..."
+        context["titulo"] = "Inicio"
+        context["mensaje"] = "Bienvenidos al portal de publicaciones!"
         return context
 
 class PublicacionListView(ListView):
@@ -20,59 +19,3 @@ class PublicacionDetailView(DetailView):
     template_name = 'publicaciones/publicacion_detail.html'
     context_object_name = 'publicacion'
     pk_url_kwarg = 'publicacion_id'
-# ---------------------------------------------------------------------------
-# InicioView
-# ---------------------------------------------------------------------------
-# TODO: Implementar InicioView usando TemplateView.
-#
-# Requisitos:
-#   - template_name = "publicaciones/inicio.html"
-#   - Sobreescribir get_context_data() para agregar al contexto:
-#       "titulo"  → str con el nombre del portal
-#       "mensaje" → str de bienvenida
-#
-# Pista:
-#   class InicioView(TemplateView):
-#       template_name = "..."
-#
-#       def get_context_data(self, **kwargs):
-#           context = super().get_context_data(**kwargs)
-#           context["titulo"] = "..."
-#           context["mensaje"] = "..."
-#           return context
-
-
-# ---------------------------------------------------------------------------
-# PublicacionListView
-# ---------------------------------------------------------------------------
-# TODO: Implementar PublicacionListView usando ListView.
-#
-# Requisitos:
-#   - model = Publicacion
-#   - context_object_name = "publicacion_list"
-#     (el template accede a esta variable con {% for pub in publicacion_list %})
-#
-# Pista:
-#   class PublicacionListView(ListView):
-#       model = ...
-#       context_object_name = "..."
-
-
-# ---------------------------------------------------------------------------
-# PublicacionDetailView
-# ---------------------------------------------------------------------------
-# TODO: Implementar PublicacionDetailView usando DetailView.
-#
-# Requisitos:
-#   - model = Publicacion
-#   - context_object_name = "publicacion"
-#     (el template accede a esta variable con {{ publicacion.titulo }})
-#   - pk_url_kwarg = "publicacion_id"
-#     (indica que el parámetro en la URL se llama "publicacion_id", no "pk")
-#   - Si no existe la publicación → responde automáticamente con 404
-#
-# Pista:
-#   class PublicacionDetailView(DetailView):
-#       model = ...
-#       context_object_name = "..."
-#       pk_url_kwarg = "..."
